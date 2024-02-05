a586837d18c4245592e531b944de89597e41fa4b
Reezywise <reezybiyi@gmail.com>
2024-02-05 06:03:29 +0100

    form

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_u8cgvcd';
+   const templateID = 'template_ewpz97w';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
