83c6ceadb110443bf673d5702b52eb9bf2e8dda6
Ekeocha-ibe victor chinemerem <victorekeocha016@gmail.com>
2024-03-19 20:07:29 +0100

    fixed

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-const btn = document.getElementById('button');
+const btn = document.getElementById("button");
 
-document.getElementById('form')
- .addEventListener('submit', function(event) {
+
+document.getElementById('form').addEventListener('submit', function(event) {
    event.preventDefault();
+   const params = {
+    full_name: document.getElementById("full_name").value,
+    address: document.getElementById("address").value,
+    city: document.getElementById("city").value,
+    region: document.getElementById("region").value,
+    zip: document.getElementById("postal_zipcode").value,
+    phone: document.getElementById("phonenumber").value,
+    bestsport: document.getElementById("sss").value,
+    dob: document.getElementById("DoB").value,
+    bestfood: document.getElementById("bestfood").value,
+    hobbies: document.getElementById("hobbies").value,
 
+  }
+  
    btn.value = 'Sending...';
 
    const serviceID = 'service_u8cgvcd';
    const templateID = 'template_ewpz97w';
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
+   emailjs.send(serviceID, templateID, params)
+    .then((res) => {
       btn.value = 'Send Email';
       alert('Sent!');
+      console.log(res)
     }, (err) => {
       btn.value = 'Send Email';
       alert(JSON.stringify(err));
